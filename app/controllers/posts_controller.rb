6dccd3ec3a0ae52fc87252294890fe83463674fd
nathalie <nathalie@fitlab.fr>
2022-03-23 11:36:08 +0100

    delete ok, no confirmation message

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,9 +23,15 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    redirect_to posts_path(post.id)
+    redirect_to posts_path
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+  
   private
   def post_params
     params.require(:post).permit(:name, :content)
